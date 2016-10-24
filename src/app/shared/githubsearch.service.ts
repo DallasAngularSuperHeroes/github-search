ccecf8463a6397910f365d0068655fed1e2d07df
r7lemieux <richard.lemieux@gmail.com>
2016-10-24 18:57:55 -0500

    cache all

--- src/app/shared/githubsearch.service.ts
+++ src/app/shared/githubsearch.service.ts
@@ -19,82 +19,108 @@ export class GithubsearchService {
     this.initCache();
   }
 
-  initCache()  {
-      this.http.get('./fallbackData.json').toPromise().then((response: Response) => {
-        GithubsearchService.cache = response.json();
-      });
+  initCache() {
+    this.http.get('./fallbackData.json').toPromise().then((response: Response) => {
+      GithubsearchService.cache = response.json();
+    });
   }
 
   private getUserUrl(userid: string) {
     return `${this.githubProfileUrl}/${userid}`;
   }
 
-  getUserProfile(userid: string) {
-    const url = this.getUserUrl(userid);
-    if (GithubsearchService.cache[userid]) {
-      let project: ReplaySubject<any> = new ReplaySubject(1);
-       project.next(GithubsearchService.cache[userid].userProfile);
-       project.complete();
-      return project.toPromise();
-    }
-    return this.http.get(url)
-      .toPromise()
-      .then((response: Response) => {
-        var profile = response.json();
-        console.log('profile.login ' + profile.login);
-        if (!profile.login) {
-          return false;
-        }
-        console.log('profile ' + profile);
 
-        GithubsearchService.cache[userid] = {userProfile: {}};
-        ['avatar_url', 'name', 'login', 'company', 'userid', 'location', 'email', 'blog', 'created_at'].forEach(field =>
-          GithubsearchService.cache[userid].userProfile[field] = profile[field]
-        );
-        return profile;
-      })
+  private tryGetFromCache(userid: string, topic: string): Promise<any> {
+    let cacheData = GithubsearchService.cache[userid]
+    if (cacheData) {
+      cacheData = cacheData[topic];
+    }
+    if (cacheData) {
+      let project: ReplaySubject<any> = new ReplaySubject(1);
+
+      project.next(cacheData);
+      project.complete();
+      return project.toPromise();
+    } else {
+      return Promise.reject(false);
+    }
+  }
+
+  getUserProfile(userid: string): Promise<any> {
+    const url = this.getUserUrl(userid);
+
+    return this.tryGetFromCache(userid, 'userProfile')
+      .catch(() => this.http.get(url).toPromise()
+        .then((response: Response) => {
+          const profile = response.json();
+          console.log('profile.login ' + profile.login);
+          if (!profile.login) {
+            return false;
+          }
+          console.log('profile ' + profile);
+
+          GithubsearchService.cache[userid] = {userProfile: {}};
+          ['avatar_url', 'name', 'login', 'company', 'userid', 'location', 'email', 'blog', 'created_at'].forEach(field =>
+            GithubsearchService.cache[userid].userProfile[field] = profile[field]
+          );
+          return profile;
+        })
+      );
   }
 
   getRepos(userid: string) {
     const url = this.getUserUrl(userid) + this.githubRepos;
-    if (GithubsearchService.cache[userid] && GithubsearchService.cache[userid].repos) {
-      let project: ReplaySubject<any> = new ReplaySubject(1);
-      project.next(GithubsearchService.cache[userid].repos);
-      project.complete();
-      return project.toPromise();
-    }
-    return this.http.get(url)
-      .toPromise()
-      .then((response: Response) => {
-        var repos = response.json();
-        console.log('repos ' + repos);
-        GithubsearchService.cache[userid].repos = repos.map(repo => {
-          return {name: repo.name, html_url: repo.html_url};
-        });
-        return repos
-      })
+
+    return this.tryGetFromCache(userid, 'repos')
+      .catch(() => this.http.get(url)
+        .toPromise()
+        .then((response: Response) => {
+          const repos = response.json();
+          console.log('repos ' + repos);
+          if (repos && repos.length) {
+            GithubsearchService.cache[userid].repos = repos.map(repo => {
+              return {name: repo.name, html_url: repo.html_url};
+            });
+          }
+          return repos
+        })
+      );
   }
 
   getFollowers(userid: string) {
     const url = this.getUserUrl(userid) + this.githubFollowers;
-    return this.http.get(url)
-      .toPromise()
-      .then((response: Response) => {
-        const followers = response.json();
-        GithubsearchService.cache[userid].followers = followers.map( user => { return {login: user.login}} );
-        return followers;
-      })
+
+    return this.tryGetFromCache(userid, 'followers')
+      .catch(() => this.http.get(url)
+        .toPromise()
+        .then((response: Response) => {
+          const followers = response.json();
+          if (followers && followers.length) {
+            GithubsearchService.cache[userid].followers = followers.map(user => {
+              return {login: user.login}
+            });
+          }
+          return followers;
+        })
+      );
   }
 
   getFollowing(userid: string) {
     const url = this.getUserUrl(userid) + this.githubFollowing;
-    return this.http.get(url)
-      .toPromise()
-      .then((response: Response) => {
-        const following = response.json();
-        GithubsearchService.cache[userid].followers = following.map( user => { return {login: user.login}} );
-        return following;
-      })
+
+    return this.tryGetFromCache(userid, 'following')
+      .catch(() => this.http.get(url)
+        .toPromise()
+        .then((response: Response) => {
+          const following = response.json();
+          if (following && following.length) {
+            GithubsearchService.cache[userid].following = following.map(user => {
+              return {login: user.login}
+            });
+          }
+          return following;
+        })
+      );
   }
 
   public showCache() {
